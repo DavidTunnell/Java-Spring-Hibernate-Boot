92e3a12c0b6c8ae23fa711773d5a73f67e4f1c2d
David Tunnell <davidtunnell9@gmail.com>
2022-01-28 18:38:05 -0600

    made bean scope demo showing singleton vs prototype

---
+++ spring-demo-one/src/com/davidtunnell/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,24 @@
+package com.davidtunnell.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		Coach theCoach2 = context.getBean("myCoach", Coach.class);
+		
+		//check if they are the same beans
+		boolean result = (theCoach == theCoach2);
+		
+		System.out.println("\nPointing to the same object?: " + result);
+		System.out.println("\nMemory location for theCoach: " + theCoach);
+		System.out.println("\nMemory location for theCoach2: " + theCoach2);
+
+		context.close();
+
+	}
+
+}
