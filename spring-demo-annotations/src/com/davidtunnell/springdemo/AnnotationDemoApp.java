14504c3f29958a00b4454d5cb4ed707135e8c4a5
David Tunnell <davidtunnell9@gmail.com>
2022-02-04 17:05:35 -0600

    added example using Spring Annotations to get/manage beans

---
+++ spring-demo-annotations/src/com/davidtunnell/springdemo/AnnotationDemoApp.java
@@ -0,0 +1,18 @@
+package com.davidtunnell.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp {
+
+	public static void main(String[] args) {
+		//read spring config file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		//get the bean from spring container
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		//call method on bean
+		System.out.println(theCoach.getDailyWorkout());
+		//close container
+		context.close();
+	}
+
+}
